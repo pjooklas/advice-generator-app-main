91f2402607d3d8ca42aefbac1c2b711939448900
pjooklas <pjooklas@users.noreply.github.com>
2022-06-12 21:21:08 +0300

    js fetch and initial css

---
+++ app.js
@@ -0,0 +1,20 @@
+'use strict';
+const adviceID = document.getElementById("id");
+const advice = document.getElementById("advice");
+
+const fetchAdvice = () => {    
+ fetch("https://api.adviceslip.com/advice") 
+  .then(response => response.json()) 
+  .then(data => {
+    adviceID.innerHTML = `<h1>${data.slip.id}</h1>`;
+    advice.innerHTML = `<p>${data.slip.advice}</p>`;
+
+
+  })
+  .catch(error => console.error(error));
+}
+
+fetchAdvice();
+
+
+
